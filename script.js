9ae088dda9a2ec1526e5ebeb48e95a5abdca096b
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2020-05-06 17:38:12 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const daysElement = document.getElementById('days');
+const hoursElement = document.getElementById('hours');
+const minutesElement = document.getElementById('minutes');
+const secondsElement = document.getElementById('seconds');
+
+const getTimeDifference = (targetDate) => {
+  let diff = targetDate.getTime() - Date.now();
+
+  if (diff < 0) {
+    return {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    };
+  }
+
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(diff / (1000 * 60 * 60)) % 24;
+  const minutes = Math.floor(diff / (1000 * 60)) % 60;
+  const seconds = Math.floor(diff / 1000) % 60;
+
+  return { days, hours, minutes, seconds };
+};
+
+const updateTime = (targetDate) => {
+  const { days, hours, minutes, seconds } = getTimeDifference(targetDate);
+  daysElement.innerText = days;
+  hoursElement.innerText = hours.toString().padStart(2, '0');
+  minutesElement.innerText = minutes.toString().padStart(2, '0');
+  secondsElement.innerText = seconds.toString().padStart(2, '0');
+};
+const targetDate = new Date('2020-05-11 09:00:00');
+updateTime(targetDate);
+setInterval(updateTime, 1000, targetDate);
